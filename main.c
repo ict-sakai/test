22c0f2ae7fda5e990039cc01e3a42916b5813150
酒井　明啓 <e1791saka@ssh.cs.ehime-u.ac.jp>
2022-07-13 11:01:54 +0900

    0から100まで表示するので、mallocで確保する記憶領域を101に修正

--- main.c
+++ main.c
@@ -15,7 +15,7 @@ int myRoot(double x) // x の平方根を返す関数。x >= 0 ではないと�
 int main(void)
 {
 	int i;
-	double *x = (double *)malloc(sizeof(double) * 100);
+	double *x = (double *)malloc(sizeof(double) * 101);
 
 	i = 0;
 	while( i <= 100 ){
