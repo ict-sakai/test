4e3a73c5058bb290a62bb6f78156e5e9cfe42108
酒井　明啓 <e1791saka@ssh.cs.ehime-u.ac.jp>
2022-07-13 11:30:19 +0900

    strpo()の関数文字列の位置を返すので、見つけた文字列を返すように、if()を追加した

--- a.php
+++ a.php
@@ -2,9 +2,13 @@
 <?php 
   foreach(file('/usr/share/dict/linux.words') as $line){
     // strpos() 関数を使って、文字列 "abc" が含まれる単語を echo "$line"; で表示しなさい。
-    $pos = strpos($line, 'abc');
-    if($pos !== false){
-	echo "$line";
+	$ary = strpos($line, 'abc');
+	/* strpos()は文字列を見つけた時に見つけた位置を返す
+			位置ではなくて、見つけた文字列を表示したいので、
+			見つからなかった時のfalse以外をecho "$line"とする 
+	*/
+	if($ary !== false){			//　
+		echo "$line";
     }
   } 
 ?>
